f6d42db4079a0f621122fbfa1f3739b6c255eb55
Eric S.R <eric7dev@gmail.com>
2018-10-27 12:00:06 -0600

    first

---
+++ runme.daemon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Bashery
+
+sysinfo="$(uname -a)"
+echo -e "\n~ bash! bash! \e[33;01m*clink*\e[0m ... BASH!!\n"
+sleep 1
+
+echo -e "BoxInfo// "$sysinfo
+echo "User// "$(whoami)
+
+echo
